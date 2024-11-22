f160d7fbceada7e61d52a27190cfeb21b94ed238
flamebarke <shain.lakin@protonmail.com>
2024-11-22 16:33:42 +1100

    add ranges

---
+++ get-cdn-ranges.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://kaeferjaeger.gay/?zip=cdn-ranges/ -O cdn-ranges.zip
+sleep 1
+unzip cdn-ranges.zip
+sleep 1
+rm cdn-ranges.zip
---
+++ get-ranges.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget https://kaeferjaeger.gay/sni-ip-ranges/amazon/ipv4_merged_sni.txt -O ipv4_merged_sni_amazon.txt
+wget https://kaeferjaeger.gay/sni-ip-ranges/digitalocean/ipv4_merged_sni.txt -O ipv4_merged_sni_digitalocean.txt
+wget https://kaeferjaeger.gay/sni-ip-ranges/google/ipv4_merged_sni.txt -O ipv4_merged_sni_google.txt
+wget https://kaeferjaeger.gay/sni-ip-ranges/microsoft/ipv4_merged_sni.txt -O ipv4_merged_sni_microsoft.txt
+wget https://kaeferjaeger.gay/sni-ip-ranges/oracle/ipv4_merged_sni.txt -O ipv4_merged_sni_oracle.txt
+
